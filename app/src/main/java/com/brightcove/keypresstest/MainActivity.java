021ed478021dbabbc52d52df747807c854fa5ec6
Kate Kelly <firekesti@users.noreply.github.com>
2016-11-03 18:34:20 -0400

    remove unneeded dependencies for smaller app
    AppCompat bloats this from 88 KB to over 5.5 MB!

--- app/src/main/java/com/brightcove/keypresstest/MainActivity.java
+++ app/src/main/java/com/brightcove/keypresstest/MainActivity.java
@@ -1,11 +1,11 @@
 package com.brightcove.keypresstest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private TextView centerText;
 
