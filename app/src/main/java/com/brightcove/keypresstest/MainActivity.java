052d3c40013863ce6b9ee1da496bd8771ad09eab
Kate Kelly <firekesti@users.noreply.github.com>
2016-11-03 18:03:51 -0400

    display keycode on the screen when a key is pressed

--- app/src/main/java/com/brightcove/keypresstest/MainActivity.java
+++ app/src/main/java/com/brightcove/keypresstest/MainActivity.java
@@ -2,12 +2,26 @@ package com.brightcove.keypresstest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView centerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        centerText = (TextView) findViewById(R.id.center_text);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Show the keycode on the screen
+        centerText.setText("KeyCode: " + keyCode);
+
+        return super.onKeyDown(keyCode, event);
     }
 }
